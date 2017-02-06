c0dd2bc14237a90a477cd9a9c8c88ff2d0a407f6
Daniel Walsh <459dan@gmail.com>
2017-02-05 23:23:50 -0500

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,8 @@ setup(
     author='Daniel Walsh',
     author_email='459dan@gmail.com',
     packages=['txpoloniex'],
+    install_requires=[
+        'txaio>=2.6.0',
+        'Twisted>=16.6.0',
+    ],
 )
