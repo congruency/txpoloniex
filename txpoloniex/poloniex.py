495f37befcd1cd9971e2d9b89a8174712a312abb
Daniel Walsh <459dan@gmail.com>
2017-02-05 21:57:37 -0500

    Clean up poloniex.py
    * Removed unused imports
    * Cleaned up namespace

--- txpoloniex/poloniex.py
+++ txpoloniex/poloniex.py
@@ -1,34 +1,25 @@
-#!/usr/bin/env python3
-
-import json
 from functools import partial
 
-from txpoloniex.base import PoloniexBase
-from txpoloniex.queue import RateLimitMixin
-from txpoloniex.commands import *
-from txpoloniex.util import *
+from txpoloniex import base, commands, queue, util
 
-PUBLIC_API = 'https://poloniex.com/public'
-TRADING_API = 'https://poloniex.com/tradingApi'
-
-class Poloniex(PoloniexBase, RateLimitMixin):
+class Poloniex(base.PoloniexBase, queue.RateLimitMixin):
 
     MAX_REQS_PER_SECOND = 6
 
     def __init__(self, *args, **kwargs):
-        PoloniexBase.__init__(self, *args, **kwargs)
+        base.PoloniexBase.__init__(self, *args, **kwargs)
 
         self.setLimit(self.MAX_REQS_PER_SECOND)
 
         # Add all commands as functions
-        for command in PUBLIC:
-            name = format_function(command)
+        for command in commands.PUBLIC:
+            name = util.format_function(command)
             self.log.trace('Adding {name} handler', name=name)
             func = partial(self.addQueue, self.requestPublic, command)
             setattr(self, name, func)
 
-        for command in PRIVATE:
-            name = format_function(command)
+        for command in commands.PRIVATE:
+            name = util.format_function(command)
             self.log.trace('Adding {name} handler', name=name)
             func = partial(self.addQueue, self.requestPrivate, command)
             setattr(self, name, func)
